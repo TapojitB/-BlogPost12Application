07f35fdec0139a8e75011d1c224fd2b0334a1f7a
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-12-15 14:19:35 +0530

    added Hystrix Dashboard and Turbine capabilities

--- area-service/src/main/java/com/evergreenprogrammer/microservices/areaservice/AreaServiceApplication.java
+++ area-service/src/main/java/com/evergreenprogrammer/microservices/areaservice/AreaServiceApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
 
+@EnableHystrixDashboard
+@EnableTurbine
 @EnableCircuitBreaker
 @EnableDiscoveryClient(autoRegister = true)
 @SpringBootApplication
